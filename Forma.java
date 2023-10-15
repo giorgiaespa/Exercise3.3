5593b4ae704de0bf07edb124968d6888ee3e4a17
gio <giorgiaespa2.0@gmail.com>
2023-10-15 22:00:56 +0200

    Initial commit

---
+++ Forma.java
@@ -0,0 +1,13 @@
+public class Forma {
+    double base;
+    double altezza;
+
+    public Forma(double base, double altezza){
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    void calcolaArea(){
+        System.out.println("L'area è");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Rettangolo rettangolo = new Rettangolo(7.13, 3.5);
+        rettangolo.calcolaArea();
+
+        Triangolo triangolo = new Triangolo(7, 9);
+        triangolo.calcolaArea();
+    }
+}
